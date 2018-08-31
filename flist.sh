ada058ea443653c6d091308247e2a4446e9093fb
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-08-31 14:58:05 +0200

    imporove flist build

--- flist.sh
+++ flist.sh
@@ -1,38 +1,83 @@
 #!/bin/bash
-set -ex
+set -e
+
+copypkg() {
+   pkgname=$1
+   target=$2
+   for file in $(dpkg -L $pkgname); do
+      if [ -d "$file" ]; then
+         continue
+      fi
+      dirname=$(dirname $file)
+      if [[ "$dirname" == /usr/share/man* ]]; then
+         continue
+      fi
+      if [[ "$dirname" == /usr/share/doc* ]]; then
+         continue
+      fi
+      targetdir="${target}${dirname}"
+      mkdir -p "$targetdir"
+      cp $file "$targetdir"
+      if file "$file" | grep dynamic; then
+         lddcopy "$file" "$target"
+      fi
+   done
+}
 
 apt-get update
-apt-get install -y openvswitch-switch wget locales
+apt-get install -y openvswitch-switch curl git
 
-# setting up locales
-if ! grep -q ^en_US /etc/locale.gen; then
-    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-    locale-gen
+if ! which lddcopy; then
+    pushd /tmp
+    git clone --depth=1 https://github.com/maxux/lddcopy.git
+    cp /tmp/lddcopy/lddcopy.sh /usr/local/bin/lddcopy
+    chmod +x /usr/local/bin/lddcopy
+    popd
 fi
 
-wget https://dl.google.com/go/go1.11.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.11.linux-amd64.tar.gz
+if ! which go; then
+    curl https://dl.google.com/go/go1.11.linux-amd64.tar.gz > /tmp/go1.11.linux-amd64.tar.gz
+    tar -C /usr/local -xzf /tmp/go1.11.linux-amd64.tar.gz
+    export PATH=$PATH:/usr/local/go/bin
+fi
+mkdir -p /gopath
+export GOPATH=/gopath
 
-export GOPATH=$HOME/go
-mkdir -p $GOPATH/bin
-mkdir -p $GOPATH/pkg
-mkdir -p $GOPATH/src
-mkdir -p $GOPATH/src/github.com/threefoldtech
-mkdir -p /var/lib/corex/plugins
-mkdir -p /run/openvswitch
-mkdir -p /var/run/openvswitch
+OVSPLUGIN=$GOPATH/src/github.com/zero-os/openvswitch-plugin
 
-cp -r /openvswitch-plugin $GOPATH/src/github.com/threefoldtech
+go get -u -v -d github.com/zero-os/openvswitch-plugin
 
-export PATH=$PATH:/usr/local/go/bin
 
-BINARY=ovs-plugin
-
-cd $GOPATH/src/github.com/threefoldtech/openvswitch-plugin
-go build -o $BINARY
-cp $BINARY /var/lib/corex/plugins
-cp startup.toml /.startup.toml
-cp plugin.toml /.plugin.toml
+TARGET=/tmp/target-ovs
 
 mkdir -p /tmp/archives
-tar -cpzf "/tmp/archives/ovs.tar.gz" --exclude tmp --exclude dev --exclude sys --exclude proc /
+rm -rf "$TARGET"
+mkdir "$TARGET"
+mkdir -p "$TARGET/var/lib/corex/plugins"
+mkdir -p "$TARGET/bin"
+mkdir -p "$TARGET/etc/openvswitch"
+mkdir -p "$TARGET/run/openvswitch"
+mkdir -p "$TARGET/var/run/openvswitch"
+mkdir -p "$TARGET/tmp"
+
+pushd "$TARGET"
+ln -s lib lib64
+popd
+
+pushd $OVSPLUGIN
+go build -o "$TARGET/var/lib/corex/plugins/ovs-plugin"
+cp startup.toml "$TARGET/.startup.toml"
+cp plugin.toml "$TARGET/.plugin.toml"
+popd
+
+
+copypkg openvswitch-switch "$TARGET"
+copypkg openvswitch-common "$TARGET"
+pushd "$TARGET/usr/sbin"
+ln -s /usr/lib/openvswitch-switch/ovs-vswitchd
+popd
+#bash /opt/lddcopy/lddcopy.sh "/bin/mkdir" "$TARGET"
+#cp /bin/mkdir "$TARGET/bin/mkdir"
+
+cd "$TARGET"
+tar -czf /tmp/archives/ovs.tar.gz .
